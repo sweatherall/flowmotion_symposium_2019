9bb6245771ae49123915d6cf67535542314f2ef8
S.Weatherall <sweather131@gmail.com>
2019-04-26 10:54:05 -0400

    add schedule graphic + update instructors

--- src/pages/schedule/Schedule.js
+++ src/pages/schedule/Schedule.js
@@ -6,7 +6,15 @@ class Schedule extends Component {
     return (
       <div className="schedule_container content-container">
         
-        <h1 className="schedule_header">schedule is coming soon..!</h1>
+        <h1 className="schedule_header">FlowMotion Symposium 2019 Schedule</h1>
+
+        <div className="schedule_img_container">
+          <img className='schedule_img' src='./images/symposium_sched_friday.png'></img>
+        </div>
+
+        <div className="schedule_img_container">
+          <img className='schedule_img' src='./images/symposium_sched_sat.png'></img>
+        </div>
 
       </div>
     );
